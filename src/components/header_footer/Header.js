a37302bd436ad6f8b1d4a63bda86ab41458a7feb
Cletus Dias <cletus@Cletuss-MBP.fios-router.home>
2020-10-30 15:07:20 -0400

    Refactor Header to react hooks

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,70 +1,51 @@
-import React, { Component } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from "@material-ui/icons/Menu";
-import IconButton from "@material-ui/core/IconButton";
-import SideDrawer from "../header_footer/SideDrawer";
+import React, { useState, useEffect } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import SideDrawer from '../header_footer/SideDrawer';
 
-class Header extends Component {
-  state = {
-    drawerOpen: false,
-    headerShow: true
-  };
+const Header = () => {
+	const [header, setHeader] = useState(true);
+	const [drawer, setDrawer] = useState(false);
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+	const handleScroll = () => {
+		if (window.scrollY > 0) {
+			setHeader({ header: false });
+		} else {
+			setHeader((prevHeader) => !prevHeader);
+		}
+	};
 
-  handleScroll = () => {
-    if (window.scrollY > 0) {
-      this.setState({
-        headerShow: true
-      });
-    } else {
-      this.setState({
-        headerShow: false
-      });
-    }
-  };
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+	}, []);
 
-  toggleDrawer = value => {
-    this.setState({
-      drawerOpen: value
-    });
-  };
+	return (
+		<AppBar
+			position='fixed'
+			style={{
+				background: header ? '#2f2f2f' : 'transparent',
+				boxShadow: 'none',
+				padding: '10px 0px',
+			}}>
+			<Toolbar>
+				<div className='header_logo'>
+					<div className='font_righteous header_logo_venue'>The Venue</div>
+					<div className='header_logo_title'>Musical Events</div>
+				</div>
 
-  render() {
-    return (
-      <AppBar
-        position="fixed"
-        style={{
-          background: this.state.headerShow ? "#2f2f2f" : "transparent",
-          boxShadow: "none",
-          padding: "10px 0px"
-        }}
-      >
-        <Toolbar>
-          <div className="header_logo">
-            <div className="font_righteous header_logo_venue">The Venue</div>
-            <div className="header_logo_title">Musical Events</div>
-          </div>
+				<IconButton
+					aria-label='Menu'
+					color='inherit'
+					onClick={() => setDrawer(true)}>
+					<MenuIcon />
+				</IconButton>
 
-          <IconButton
-            aria-label="Menu"
-            color="inherit"
-            onClick={() => this.toggleDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          <SideDrawer
-            open={this.state.drawerOpen}
-            onClose={value => this.toggleDrawer(value)}
-          />
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+				<SideDrawer open={drawer} onClose={() => setDrawer(false)} />
+			</Toolbar>
+		</AppBar>
+	);
+};
 
 export default Header;
